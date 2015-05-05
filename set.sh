cc0628e9a8320e04b1e6cc0e72fcc80d2b3d146f
Brandon Ly <lypanda1@gmail.com>
2015-05-05 08:11:38 -0700

    added ruby proxies

--- set.sh
+++ set.sh
@@ -4,6 +4,11 @@ npm config set proxy $proxy
 npm config set https-proxy $proxy
 
 # Ruby
+	#if you're using bash instead of zsh change .zshrc to .bashrc
+echo "export ALL_PROXY=\"http://proxy.ccsd.net\"" >> ~/.zshrc
+sed -i '/unset HTTP_PROXY/d' ~/.zshrc
+sed -i '/unset http_proxy/d' ~/.zshrc
+sed -i '/unset ALL_PROXY/d' ~/.zshrc
 
 # Git
 git config --global http.proxy $proxy
--- unset.sh
+++ unset.sh
@@ -3,8 +3,11 @@ npm config delete proxy
 npm config delete https-proxy
 
 # Ruby
-unset http_proxy
-unset ALL_PROXY
+	#if you're using bash instead of zsh change .zshrc to .bashrc
+sed -i '/export ALL_PROXY/d' ~/.zshrc
+echo "unset HTTP_PROXY" >> ~/.zshrc
+echo "unset http_proxy" >> ~/.zshrc
+echo "unset ALL_proxy" >> ~/.zshrc
 
 # Git
 git config --unset --global http.proxy
