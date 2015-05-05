fc493d9b5669d742564cc442643f09d39219c430
Colton Fitzgerald <rockincolton@gmail.com>
2015-05-05 13:51:17 -0700

    Add ZSH & Bash support

--- unset.sh
+++ unset.sh
@@ -3,11 +3,19 @@ npm config delete proxy
 npm config delete https-proxy
 
 # Ruby
-	#if you're using bash instead of zsh change .zshrc to .bashrc
-sed -i '/export ALL_PROXY/d' ~/.zshrc
-echo "unset HTTP_PROXY" >> ~/.zshrc
-echo "unset http_proxy" >> ~/.zshrc
-echo "unset ALL_proxy" >> ~/.zshrc
+  if [ -f "$HOME/.zshrc" ]; then
+    sed -i '/export ALL_PROXY/d' ~/.zshrc
+    echo "unset HTTP_PROXY" >> ~/.zshrc
+    echo "unset http_proxy" >> ~/.zshrc
+    echo "unset ALL_proxy" >> ~/.zshrc
+    echo "Removed From ~/.zshrc"
+  else
+    sed -i '/export ALL_PROXY/d' ~/.bashrc
+    echo "unset HTTP_PROXY" >> ~/.bashrc
+    echo "unset http_proxy" >> ~/.bashrc
+    echo "unset ALL_proxy" >> ~/.bashrc
+    echo "Removed From ~/.bashrc"
+  fi
 
 # Git
 git config --unset --global http.proxy
