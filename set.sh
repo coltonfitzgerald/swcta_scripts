fa8d06ac45cddc9ff8c08a5338f9c73b7291d92e
Brandon Ly <lypanda1@gmail.com>
2015-05-11 19:16:56 -0700

    badoop

--- set.sh
+++ set.sh
@@ -6,7 +6,8 @@ npm config set https-proxy $proxy
 
 # Ruby
 
-if [ -f "$HOME/.zshrc" ]; then # Check if using oh-my-zsh
+if [ -f "$HOME/.zshrc" ]; # Check if using oh-my-zsh
+then 
   echo "export ALL_PROXY=\"http://proxy.ccsd.net\"" >> ~/.zshrc
   sed -i '/unset HTTP_PROXY/d' ~/.zshrc
   sed -i '/unset http_proxy/d' ~/.zshrc
